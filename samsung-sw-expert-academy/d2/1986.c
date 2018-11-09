56489de95cf91ce46285d683e7b3befd8cddb7a5
Choi Jae-won <jwonee1343@gmail.com>
2018-11-10 04:03:55 +0900

    Update 1986.c

--- samsung-sw-expert-academy/d2/1986.c
+++ samsung-sw-expert-academy/d2/1986.c
@@ -4,7 +4,7 @@ void
 solve(void)
 {
 	int num;
-
-    scanf("%d", &num);
-    printf("%.f\n", (num & 1 ? 1 : -1) * (num * 0.5 + 0.05) );
+	
+	scanf("%d", &num);
+	printf("%.f\n", (num & 1 ? 1 : -1) * (num * 0.5 + 0.05) );
 }
