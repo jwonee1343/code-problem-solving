dc5f87b5ef1bff57902a290cc80bcc3483350ed9
Choi Jae-won <jwonee1343@gmail.com>
2018-11-22 23:27:46 +0900

    Update 2072.c

--- samsung-sw-expert-academy/d1/2072.c
+++ samsung-sw-expert-academy/d1/2072.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 #define COUNT 10
+#define IS_ODD(num) (num & 1)
 
-void
-solve(void)
+void solve(void)
 {
 	int loop = COUNT;
 	int sum = 0;
 	int num;
 
-	for (; loop--; )
-	{
+	for (; loop--; ) {
 		scanf("%d", &num);
-		if (num & 1)
+		if ( IS_ODD(num) )
 			sum += num;		
 	}
 
