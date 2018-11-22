275ef3e1d33e21b9b3c8ab7db691b3f1c9195c94
Choi Jae-won <jwonee1343@gmail.com>
2018-11-22 23:24:11 +0900

    Update 2068.c

--- samsung-sw-expert-academy/d1/2068.c
+++ samsung-sw-expert-academy/d1/2068.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-int
-solve(void)
+int solve(void)
 {
 	int loop = 10;
 	int max = 0;
 	int num;
 
-	for (; loop--; )
-	{
+	for (; loop--; ) {
 		scanf("%d", &num);
 		if (max < num)
 			max = num;
 	}
-
 	printf("%d\n", max);
 }
